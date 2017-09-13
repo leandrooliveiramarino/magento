fd5f5cb2042aba2a69038ec35ca0412a485209d6
Leandro Marino <=>
2017-09-12 22:15:52 -0300

    Ajustar Módulo do Admin

--- app/code/Picstore/ExampleAdminNewPage/Controller/Adminhtml/HelloWorld/Index.php
+++ app/code/Picstore/ExampleAdminNewPage/Controller/Adminhtml/HelloWorld/Index.php
@@ -1,35 +1,36 @@
-    <?php
-      namespace Picstore\ExampleAdminNewPage\Controller\Adminhtml\HelloWorld;
+<?php
 
-      class Index extends \Magento\Backend\App\Action
-      {
-        /**
-        * @var \Magento\Framework\View\Result\PageFactory
-        */
-        protected $resultPageFactory;
+namespace Picstore\ExampleAdminNewPage\Controller\Adminhtml\HelloWorld;
 
-        /**
-         * Constructor
-         *
-         * @param \Magento\Backend\App\Action\Context $context
-         * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
-         */
-        public function __construct(
-            \Magento\Backend\App\Action\Context $context,
-            \Magento\Framework\View\Result\PageFactory $resultPageFactory
-        ) {
-             parent::__construct($context);
-             $this->resultPageFactory = $resultPageFactory;
-        }
+class Index extends \Magento\Backend\App\Action
+{
+    /**
+    * @var \Magento\Framework\View\Result\PageFactory
+    */
+    protected $resultPageFactory;
 
-        /**
-         * Load the page defined in view/adminhtml/layout/exampleadminnewpage_helloworld_index.xml
-         *
-         * @return \Magento\Framework\View\Result\Page
-         */
-        public function execute()
-        {
-             return  $resultPage = $this->resultPageFactory->create();
-        }
-      }
-    ?>
+    /**
+     * Constructor
+     *
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+         parent::__construct($context);
+         $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     * Load the page defined in view/adminhtml/layout/exampleadminnewpage_helloworld_index.xml
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+         return  $resultPage = $this->resultPageFactory->create();
+    }
+  }
+?>
--- app/code/Picstore/ExampleAdminNewPage/registration.php
+++ app/code/Picstore/ExampleAdminNewPage/registration.php
@@ -1,6 +1,6 @@
-    <?php
-    \Magento\Framework\Component\ComponentRegistrar::register(
-        \Magento\Framework\Component\ComponentRegistrar::MODULE,
-        'Picstore_ExampleAdminNewPage',
-        __DIR__
-    );
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Picstore_ExampleAdminNewPage',
+    __DIR__
+);
